5372a5845569e09ef3cc98077c464bf43bd9b7dc
Young Mayor <youngmayor08@gmail.com>
2020-04-14 19:39:27 +0100

    Schema defaultStringLength fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Tools\PaystackPay;
 use App\Tools\ReadMoni;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -30,6 +31,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Schema::defaultStringLength(191);
     }
 }
